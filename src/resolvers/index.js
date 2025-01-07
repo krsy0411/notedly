2f220c66046903acac41a79a366eeda954bf2c26
PANdas <krsy0411@naver.com>
2025-01-07 19:42:51 +0900

    [fix] 패키지 충돌 문제 해결

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,7 +2,8 @@ const Query = require('./query');
 const Mutation = require('./mutation');
 const Note = require('./note');
 const User = require('./user');
-const { GraphQLDateTime } = require('graphql-iso-date');
+// graphql-iso-Date 패키지 -> graphql-scalars로 migration(패키지 버전 충돌 문제)
+const { GraphQLDateTime } = require('graphql-scalars');
 
 module.exports = {
   Query,
